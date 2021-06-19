4802e868292fdf3c5f033be92e2f7eb0e4900b70
Brian Adams <bpeteradams@gmail.com>
2021-06-19 14:48:07 -0400

    created basic Table react component

---
+++ src/components/Table.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Heading 1</th>
+          <th>Heading 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Column 1:1</td>
+          <td>Column 1:2</td>
+        </tr>
+        <tr>
+          <td>Column 2:1</td>
+          <td>Column 2:2</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default Table;
