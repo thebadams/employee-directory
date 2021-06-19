093e7f6b4ecf6f4918ad0abd0f9f9975e0511c12
Brian Adams <bpeteradams@gmail.com>
2021-06-19 14:55:33 -0400

    wrote a basic TableBody component, imported it into the Table component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import TableHeader from './TableHeader';
+import TableBody from './TableBody';
 
 const Table = () => {
   return (
     <table>
      <TableHeader />
-      <tbody>
-        <tr>
-          <td>Column 1:1</td>
-          <td>Column 1:2</td>
-        </tr>
-        <tr>
-          <td>Column 2:1</td>
-          <td>Column 2:2</td>
-        </tr>
-      </tbody>
+     <TableBody />
     </table>
   )
 }
---
+++ src/components/TableBody.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TableBody = () => {
+  return (
+     <tbody>
+        <tr>
+          <td>Column 1:1</td>
+          <td>Column 1:2</td>
+        </tr>
+        <tr>
+          <td>Column 2:1</td>
+          <td>Column 2:2</td>
+        </tr>
+      </tbody>
+  )
+}
+
+export default TableBody;
